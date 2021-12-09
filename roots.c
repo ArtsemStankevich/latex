de84716cac39ed5e5c6a90ea53fb0b961a7a6ac2
Artsem Stankevich <artsemstankevich@gmail.com>
2021-12-09 16:18:19 +0100

    add roots

---
+++ roots.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a, b,c, b1, x1, x2, x;
+	b1 = b*b ;
+	double D = b1 - 4*a*c;
+	if (D > 0){
+	x1 = (-b + sqrt(D))/(2 * a);
+	x2 = (-b - sqrt(D))/(2 * a);
+	
+}
+    if (D == 0 ){
+	x =  -b/2*a;
+	
+}
+	if ( D < 0){
+	
+
+}
+}
