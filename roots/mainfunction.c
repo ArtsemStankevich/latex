9046d0314e536f13c4e612b512a1acdd5d8b231c
Ihor Malyi <malyyigor34@gmail.com>
2021-12-13 23:39:55 +0100

    add double types support, fix bugs and delete code duplication, add structures

--- roots/mainfunction.c
+++ roots/mainfunction.c
@@ -1,75 +1,72 @@
 #include <stdio.h>
 #include <math.h>
 
-double find_roots(double a, double b, double c, double from, double to, int k);
+struct result find_roots(double a, double b, double c);
+void print_roots(struct result results);
+void calculate(struct result *results, double from, double to, double k);
 
+struct result{
+    double a;
+    double b;
+    double c;
+
+    double d;
+
+    double roots[1];
+    int roots_num;
+
+    double from;
+    double to;
+
+    double table[50][2];
+};
 
 int main(){
-	double a=4, b=4, c=1, from=1, to=6, k=1;
-	find_roots(a, b, c, from, to, k);
-	
-	
-	
-	
-	
-	
+	double a=4.584, b=4.4, c=1, from=1.12, to=6, k=0.5;
+	struct result res = find_roots(a, b, c);
+	calculate(&res, -5, 5, 2.5);
+print_roots(res);
 }
 
-double find_roots(double a, double b, double c, double from, double to, int k)
+struct result find_roots(double a, double b, double c)
 {
-	double b1, x1, x2, x;
-	int results[6];
-	b1 = b*b ;
-	double d = b1 - 4*a*c;
+    double roots[2];
+    int roots_num=0;
+	double d = b*b - 4*a*c;
 	if (d > 0){
-	x1 = (-b + sqrt(d))/(2 * a);
-	x2 = (-b - sqrt(d))/(2 * a);
-    int results[]={a, b, c, d, b1, x1, x2};
-}
-    if (d == 0 ){
-	x =  -b/2*a;
-    int results[]={a, b, c, d, b1, x};
-	
-}
-	if ( d < 0){
-    int results[]={a, b, c, d, b1};
+        roots[0] = (-b + sqrt(d))/(2 * a);
+        roots[1] = (-b - sqrt(d))/(2 * a);
+        roots_num=2;
+    }else if(d == 0 ){
+        roots_num=1;
+        roots[0] =  -b/2*a;
+    }else{
+        roots_num=0;
+    }
+    struct result results = {a,b,c,d};
+    return results;
 }
 
-	printf("Mamy funkcje %dx^2 + %dx +%d\n", results[0], results[1], results[2]);
-	printf("\\Delta = %d^2 - 4\\times%d\\times%d\n", results[1], results[0], results[2]);
-	if(results[3]>0){
-		printf("x_1 = \frac{-%d + \\sqrt{%d}}{2\\times%d} \n", results[1], results[3], results[0]);
-		printf("x_2 = \frac{-%d - \\sqrt{%d}}{2\\times%d} \n", results[1], results[3], results[0]);
-		printf("x_1 = %d\n", results[5]);
-		printf("x_2 = %d\n", results[6]);
-	}
-	if(results[3]==0){
-		printf("x = \\frac{-%d}{2\\times%d} \n", results[1], results[0]);
-		printf("x = %d\n", results[5]);	
-	}
-	if(results[3]<0){
-		printf("Niema pierwiastkow\n");	
-	}
-	
-	
-		printf("\\begin{tabular}{|c|c|} \\hline \n");
-	int el1 = 0;
-	int el2 = 0;
-    int tab[el1][el2];
-	
-	for(int i=1; i<=6; i++){
-		int x=i;
-		int y=4*i*i + 4*i + 1;
-		tab[el1][el2] = x;
-		tab[el1][el2+1] = y;
-		el1++;
-		printf("%d & %d \\ \\hline\n", tab[el1][el2], tab[el1][el2+1]);
+void calculate(struct result *results, double from, double to, double k){
+    double x=from;
+    for(int i=0;i<=to;i++){
+        results->table[i][0]= x;
+        results->table[i][1]= x*x*results->a+x*results->b+results->c;
+        x+=k;
+    }
+}
 
-	}
 
-	printf("\\end{tabular}\n");
-	
-	
-	
-	
+void print_latex(struct result results){
+
+    //use structure definition write function for print result in latex markup;
+    //THERE
+
+   printf("a=%lfb=%lf, root1=%lf", results.a, results.b, results.table[0][0]); //example
+
+
+   //example for print table
+   for(int i=0;i<=to;i++){
+    printf("%lf %lf\n", results->table[i][0],  results->table[i][1]);
+    }
 }
