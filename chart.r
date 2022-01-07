f74c6ddb29b32d2c4fabd5f74bf1b023fd5e2968
mcurylo <80968468+mcurylo@users.noreply.github.com>
2022-01-07 17:06:47 +0100

    Add files via upload

---
+++ chart.r
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+args <- commandArgs(TRUE)
+#qdir<-Sys.getenv("VARIABLENAME")
+#pathname<-qdir[1]
+## Read the input data
+a<-read.table(args[1])
+## Set the output file name/type
+#dir.create(paste0("result/charts_tmp"))
+png(file=paste0("res/",args[2],".png"))
+## Plot your data
+par(bg=toString(a$V6))
+curve(a$V1*x^2+a$V2*x+a$V3, from=a$V4, to=a$V5, main=paste(toString(a$V1),"x^2 + ",toString(a$V2),"x + ",toString(a$V3)), xlab="x", ylab=toString("y"))
+## Close the graphics device (write to the output file)
+dev.off()
